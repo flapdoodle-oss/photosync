e7b19124d227f62681ee8ebad15020b50efc6793
Michael Mosmann <michael@mosmann.de>
2020-01-11 15:11:24 +0100

    tests

--- src/test/java/de/flapdoodle/photosync/diff/ScanDiffAnalyzerTest.kt
+++ src/test/java/de/flapdoodle/photosync/diff/ScanDiffAnalyzerTest.kt
@@ -4,28 +4,24 @@ import de.flapdoodle.photosync.Blob
 import de.flapdoodle.photosync.FileTimes
 import de.flapdoodle.photosync.MockedHasher
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.nio.file.Path
 
 internal class ScanDiffAnalyzerTest {
 
   @Test
-  fun `src contains all, dst nothing`() {
+  fun `src contains all, dst nothing - create entry`() {
     val srcGroup = GroupedBlobs(listOf(
         BlobWithMeta(Blob(Path.of("src", "bar"), 0, FileTimes.now()))
     ))
 
-    val src = Scan(Path.of("src"),
-        listOf(srcGroup)
-    )
-
+    val src = Scan(Path.of("src"), listOf(srcGroup))
     val dst = Scan(Path.of("dst"), emptyList())
 
     val hasher = MockedHasher()
-        .addRule(Path.of("src","bar"),0,"hash")
+        .addRule(Path.of("src", "bar"), 0, "hash")
 
-    val result = ScanDiffAnalyzer.scan(src,dst,hasher)
+    val result = ScanDiffAnalyzer.scan(src, dst, hasher)
 
     assertThat(result).size().isEqualTo(1)
         .returnToIterable()
@@ -35,5 +31,59 @@ internal class ScanDiffAnalyzerTest {
         .isEqualTo(srcGroup)
   }
 
+  @Test
+  fun `dst contains all, src nothing - delete entry`() {
+    val dstGroup = GroupedBlobs(listOf(
+        BlobWithMeta(Blob(Path.of("dst", "bar"), 0, FileTimes.now()))
+    ))
+
+    val src = Scan(Path.of("src"), emptyList())
+    val dst = Scan(Path.of("dst"), listOf(dstGroup))
+
+    val hasher = MockedHasher()
+        .addRule(Path.of("dst", "bar"), 0, "hash")
+
+    val result = ScanDiffAnalyzer.scan(src, dst, hasher)
+
+    assertThat(result).size().isEqualTo(1)
+        .returnToIterable()
+        .element(0)
+        .isInstanceOf(DiffEntry.DeletedEntry::class.java)
+        .extracting { (it as DiffEntry.DeletedEntry).dst }
+        .isEqualTo(dstGroup)
+  }
+
+  @Test
+  fun `src contains same as dst`() {
+    val srcGroup = GroupedBlobs(listOf(
+        BlobWithMeta(Blob(Path.of("src", "bar"), 0, FileTimes.now()))
+    ))
+
+    val dstGroup = GroupedBlobs(listOf(
+        BlobWithMeta(Blob(Path.of("dst", "baz"), 0, FileTimes.now()))
+    ))
+
+    val src = Scan(Path.of("src"), listOf(srcGroup))
+    val dst = Scan(Path.of("dst"), listOf(dstGroup))
+
+    val hasher = MockedHasher()
+        .addRule(Path.of("src", "bar"), 0, "hash")
+        .addRule(Path.of("dst", "baz"), 0, "hash")
+
+    val result = ScanDiffAnalyzer.scan(src, dst, hasher)
+
+    assertThat(result).size().isEqualTo(2)
+        .returnToIterable()
+        .anySatisfy {
+          assertThat(it)
+              .isInstanceOf(DiffEntry.Match::class.java)
+              .extracting { (it as DiffEntry.Match) }
+              .isEqualTo(DiffEntry.Match(srcGroup, dstGroup))
+        }
+        .anySatisfy {
+          assertThat(it)
+              .isInstanceOf(DiffEntry.Noop::class.java)
+        }
+  }
 
 }
