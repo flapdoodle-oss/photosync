d89523e73ac632b5150d41e1439345a6510030c0
Michael Mosmann <michael@mosmann.de>
2020-01-28 17:45:17 +0100

    command output changed

--- src/main/kotlin/de/flapdoodle/photosync/sync/UnixCommandListRenderer.kt
+++ src/main/kotlin/de/flapdoodle/photosync/sync/UnixCommandListRenderer.kt
@@ -4,32 +4,53 @@ import java.nio.file.Path
 
 object UnixCommandListRenderer : CommandExecutor {
   override fun execute(commands: List<Command>) {
-    commands.forEach {
+    val removeCommands = commands.filter { it is Command.Remove }
+    val copyBackCommands = commands.filter { it is Command.CopyBack }
+    val moveAndCopy = commands - removeCommands - copyBackCommands
+
+    require(moveAndCopy.size + removeCommands.size + copyBackCommands.size == commands.size) {
+      "filtering is wrong for $commands"
+    }
+
+    moveAndCopy.forEach {
       when (it) {
         is Command.Copy -> printCommand("cp --preserve=timestamps", it.src, it.dst)
-        is Command.CopyBack -> printCommand("#cp-back --preserve=timestamps", it.src, it.dst)
+//        is Command.CopyBack -> {}
         is Command.Move -> printCommand("mv", it.src, it.dst)
         is Command.MkDir -> printCommand("mkdir", it.dst)
         is Command.BulkMove -> printCommand("mv", it.src.resolve("*"), it.dst)
-        is Command.Remove -> { }
+//        is Command.Remove -> { }
       }
     }
 
-    println("---------------------------")
-    println("unused image copies")
-    println("---------------------------")
-    commands.forEach {
-      when (it) {
-        is Command.Remove -> if (it.cause == Command.Cause.CopyRemovedFromSource) printCommand("rm", it.dst)
+    if (copyBackCommands.isNotEmpty()) {
+      println("---------------------------")
+      println("destination is newer")
+      println("---------------------------")
+      copyBackCommands.forEach {
+        when (it) {
+          is Command.CopyBack -> printCommand("cp --preserve=timestamps", it.dst, it.src)
+        }
       }
     }
 
-    println("---------------------------")
-    println("removed source images")
-    println("---------------------------")
-    commands.forEach {
-      when (it) {
-        is Command.Remove -> if (it.cause == Command.Cause.DeletedEntry) printCommand("rm", it.dst)
+    if (removeCommands.isNotEmpty()) {
+      println("---------------------------")
+      println("unused image copies")
+      println("---------------------------")
+      removeCommands.forEach {
+        when (it) {
+          is Command.Remove -> if (it.cause == Command.Cause.CopyRemovedFromSource) printCommand("rm", it.dst)
+        }
+      }
+
+      println("---------------------------")
+      println("removed source images")
+      println("---------------------------")
+      removeCommands.forEach {
+        when (it) {
+          is Command.Remove -> if (it.cause == Command.Cause.DeletedEntry) printCommand("rm", it.dst)
+        }
       }
     }
   }
