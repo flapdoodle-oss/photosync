7a89f50e851ff6a24ed111c591e3f5fb948e0628
Michael Mosmann <michael@mosmann.de>
2020-09-19 09:08:58 +0200

    kotlin 1.4.10

--- src/main/kotlin/de/flapdoodle/dirsync/ui/views/StartView.kt
+++ src/main/kotlin/de/flapdoodle/dirsync/ui/views/StartView.kt
@@ -5,6 +5,11 @@ import tornadofx.View
 import tornadofx.hbox
 
 class StartView : View("DirSync") {
+    init {
+        primaryStage.width = 1024.0
+        primaryStage.height = 768.0
+    }
+    
     override val root = hbox {
         
     }
