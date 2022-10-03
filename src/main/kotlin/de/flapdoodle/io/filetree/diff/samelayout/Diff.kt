ec5afa582b53e5ee42a7fee445cb2e0590ae5fbf
Michael Mosmann <michael@mosmann.de>
2022-10-03 20:13:45 +0200

    node diff

--- src/main/kotlin/de/flapdoodle/io/filetree/diff/samelayout/Diff.kt
+++ src/main/kotlin/de/flapdoodle/io/filetree/diff/samelayout/Diff.kt
@@ -2,6 +2,7 @@ package de.flapdoodle.io.filetree.diff.samelayout
 
 import de.flapdoodle.io.filetree.Node
 import de.flapdoodle.photosync.filehash.Hash
+import de.flapdoodle.photosync.filehash.Hasher
 import java.nio.file.Path
 
 data class Diff(
@@ -20,11 +21,11 @@ data class Diff(
             val dest: Node
         ) : Entry()
 
-        data class HashChanged(
+        data class HashChanged<T: Hash<T>>(
             val src: Node.File,
             val dest: Node.File,
-            val srcHash: Hash<Any>,
-            val destHash: Hash<Any>
+            val srcHash: T,
+            val destHash: T
         ) : Entry()
 
         data class DirectoryChanged(
@@ -35,11 +36,11 @@ data class Diff(
     }
 
     companion object {
-        fun diff(src: Node.Top, dest: Node.Top): Diff {
-            return Diff(src.path, dest.path, diff(src.children, dest.children))
+        fun <T: Hash<T>> diff(src: Node.Top, dest: Node.Top, hasher: Hasher<T>): Diff {
+            return Diff(src.path, dest.path, diff(src.path, src.children, dest.path, dest.children, hasher))
         }
 
-        fun diff(src: List<Node>, dest: List<Node>): List<Diff.Entry> {
+        fun <T: Hash<T>> diff(srcPath: Path, src: List<Node>, destPath: Path, dest: List<Node>, hasher: Hasher<T>): List<Entry> {
             val srcByName = src.associateBy(Node::name)
             val destByName = dest.associateBy(Node::name)
 
@@ -50,7 +51,7 @@ data class Diff(
             val onlyInSrc = srcByName.keys - destByName.keys
             val onlyInDest = destByName.keys - srcByName.keys
 
-            val changed = both.map { diffNodes(srcByName[it]!!, destByName[it]!!) }
+            val changed = both.map { diffNodes(srcPath, srcByName[it]!!, destPath, destByName[it]!!, hasher) }
 
             val missing = src.filter { onlyInSrc.contains(it.name) }
                 .map { Entry.Missing(it) }
@@ -61,25 +62,34 @@ data class Diff(
             return missing + removed + changed
         }
 
-        private fun diffNodes(src: Node, dest: Node): Entry {
+        private fun <T: Hash<T>> diffNodes(srcPath: Path, src: Node, destPath: Path, dest: Node, hasher: Hasher<T>): Entry {
             require(src.name == dest.name) { "different names: $src - $dest" }
 
             if (src::class != dest::class) {
                 return Entry.TypeMismatch(src, dest)
             }
             return when (src) {
-                is Node.File -> diff(src, dest as Node.File)
+                is Node.File -> diff(srcPath, src, destPath, dest as Node.File, hasher)
                 is Node.SymLink -> diff(src, dest as Node.SymLink)
-                is Node.Directory -> diff(src, dest as Node.Directory)
+                is Node.Directory -> diff(srcPath, src, destPath, dest as Node.Directory, hasher)
             }
         }
 
-        private fun diff(src: Node.File, dest: Node.File): Entry {
-            // TODO check hash
-            return if (src == dest)
-                Entry.IsEqual(src)
-            else
-                Entry.Changed(src, dest)
+        private fun <T: Hash<T>> diff(srcPath: Path, src: Node.File, destPath: Path, dest: Node.File, hasher: Hasher<T>): Entry {
+            return if (src.size == dest.size) {
+                // check for content change
+                val srcHash = hasher.hash(srcPath.resolve(src.name), src.size)
+                val destHash = hasher.hash(destPath.resolve(dest.name), dest.size)
+                if (srcHash == destHash) {
+                    // same content
+                    if (src == dest) Entry.IsEqual(src) else
+                        Entry.Changed(src,dest)
+                } else {
+                    Entry.HashChanged(src,dest,srcHash,destHash)
+                }
+            } else {
+                Entry.Changed(src,dest)
+            }
         }
 
         private fun diff(src: Node.SymLink, dest: Node.SymLink): Entry {
@@ -89,11 +99,11 @@ data class Diff(
                 Entry.Changed(src, dest)
         }
 
-        private fun diff(src: Node.Directory, dest: Node.Directory): Entry {
+        private fun <T: Hash<T>> diff(srcPath: Path, src: Node.Directory, destPath: Path, dest: Node.Directory, hasher: Hasher<T>): Entry {
             return if (src == dest)
                 Entry.IsEqual(src)
             else
-                Entry.DirectoryChanged(src, dest, diff(src.children, dest.children))
+                Entry.DirectoryChanged(src, dest, diff(srcPath.resolve(src.name), src.children, destPath.resolve(dest.name), dest.children, hasher))
         }
     }
 }
--- src/test/kotlin/de/flapdoodle/io/filetree/diff/samelayout/DiffTest.kt
+++ src/test/kotlin/de/flapdoodle/io/filetree/diff/samelayout/DiffTest.kt
@@ -1,8 +1,12 @@
 package de.flapdoodle.io.filetree.diff.samelayout
 
 import de.flapdoodle.io.filetree.Node
+import de.flapdoodle.io.layouts.MockedHash
 import de.flapdoodle.photosync.LastModified
+import de.flapdoodle.photosync.MockedHasher
 import de.flapdoodle.types.Either
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.nio.file.Path
 
@@ -12,11 +16,13 @@ internal class DiffTest {
     fun sample() {
         val now = LastModified.now()
 
-        val src = Node.Top(Path.of("src"), now, children = listOf(
+        val srcPath = Path.of("src")
+        val srcTree = Node.Top(
+            srcPath, now, children = listOf(
             Node.File("same-file", now, 123L),
             Node.File("changed-size", now, 100L),
             Node.File("changed-time", now + 1, 100L),
-            Node.File("changed-hash", now + 1, 200L),
+            Node.File("changed-hash", now + 1, 100L),
 
             Node.File("new-file", now + 1, 10L),
 
@@ -27,11 +33,13 @@ internal class DiffTest {
             ))
         ))
 
-        val dest = Node.Top(Path.of("src"), now, children = listOf(
+        val destPath = Path.of("dest")
+        val destTree = Node.Top(
+            destPath, now, children = listOf(
             Node.File("same-file", now, 123L),
             Node.File("changed-size", now, 200L),
-            Node.File("changed-time", now, 200L),
-            Node.File("changed-hash", now + 1, 200L),
+            Node.File("changed-time", now, 100L),
+            Node.File("changed-hash", now + 1, 100L),
 
             Node.File("removed-file", now -10, 10L),
 
@@ -42,19 +50,53 @@ internal class DiffTest {
             ))
         ))
 
-        val diff = Diff.diff(src, dest)
-        printDiff(diff.entries)
-    }
+        val hasher = MockedHasher()
+            .addRule(srcPath.resolve("same-file"), 123L, "same-file-hash")
+            .addRule(destPath.resolve("same-file"), 123L, "same-file-hash")
+            .addRule(srcPath.resolve("changed-time"), 100L, "changed-time-hash")
+            .addRule(destPath.resolve("changed-time"), 100L, "changed-time-hash")
+            .addRule(srcPath.resolve("changed-hash"), 100L, "hash#1")
+            .addRule(destPath.resolve("changed-hash"), 100L, "hash#2")
+            .addRule(srcPath.resolve("sub").resolve("file"), 123L, "same-file-hash")
+            .addRule(destPath.resolve("sub").resolve("file"), 123L, "same-file-hash")
 
-    private fun printDiff(entries: List<Diff.Entry>) {
-        entries.forEach {
-            when (it) {
-                is Diff.Entry.DirectoryChanged -> {
-                    println("--> ${it.src} ? ${it.dest}")
-                    printDiff(it.entries)
-                }
-                else -> println(" -> $it")
-            }
-        }
+        val diff = Diff.diff(srcTree, destTree, hasher)
+
+        assertThat(diff.src).isEqualTo(srcPath)
+        assertThat(diff.dest).isEqualTo(destPath)
+        assertThat(diff.entries)
+            .hasSize(8)
+            .contains(Diff.Entry.IsEqual(node = Node.File("same-file", now, 123L)))
+            .contains(Diff.Entry.Changed(
+                src = Node.File("changed-size", now, 100L),
+                dest = Node.File("changed-size", now, 200L)
+            ))
+            .contains(Diff.Entry.Changed(
+                src = Node.File("changed-time", now + 1, 100L),
+                dest = Node.File("changed-time", now, 100L)
+            ))
+            .contains(Diff.Entry.HashChanged<MockedHasher.MockHash>(
+                src = Node.File("changed-hash", now + 1, 100L),
+                dest = Node.File("changed-hash", now + 1, 100L),
+                srcHash = MockedHasher.MockHash("hash#1"),
+                destHash = MockedHasher.MockHash("hash#2")
+            ))
+            .contains(Diff.Entry.Missing(src=Node.File("new-file", now + 1, 10L)))
+            .contains(Diff.Entry.Removed(dest = Node.File("removed-file", now - 10, 10L)))
+            .contains(Diff.Entry.Changed(
+                src = Node.SymLink("changed-sym-link", now, Either.left(Node.NodeReference(listOf("same-file")))),
+                dest = Node.SymLink("changed-sym-link", now + 1, Either.left(Node.NodeReference(listOf("same-file")))),
+            ))
+            .contains(Diff.Entry.DirectoryChanged(
+                src = Node.Directory("sub", now, children = listOf(
+                    Node.File("file", now, 123L),
+                )),
+                dest = Node.Directory("sub", now + 1, children = listOf(
+                    Node.File("file", now, 123L),
+                )),
+                entries = listOf(
+                    Diff.Entry.IsEqual(node = Node.File("file", now, 123L))
+                )
+            ))
     }
 }
