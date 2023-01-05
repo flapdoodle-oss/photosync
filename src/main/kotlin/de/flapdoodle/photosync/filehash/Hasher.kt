95b5587cb8c300dc2fece9bebe7d5049d206d8a6
Michael Mosmann <michael@mosmann.de>
2023-01-05 17:32:16 +0100

    minor

--- src/main/kotlin/de/flapdoodle/photosync/filehash/Hasher.kt
+++ src/main/kotlin/de/flapdoodle/photosync/filehash/Hasher.kt
@@ -1,19 +1,11 @@
 package de.flapdoodle.photosync.filehash
 
-import de.flapdoodle.photosync.progress.Monitor
-import de.flapdoodle.photosync.progress.Statistic
 import java.nio.file.Path
 
 interface Hasher<T: Hash<T>> {
   fun hash(path: Path, size: Long): T
 
   fun withMonitor(): Hasher<T> {
-    val that = this
-    return object: Hasher<T> {
-      override fun hash(path: Path, size: Long): T {
-        Monitor.message("$path")
-        return that.hash(path,size)
-      }
-    }
+    return MonitoringHasher(this)
   }
 }
